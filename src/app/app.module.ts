b72ec81e36b06f9262732ca4f10b18020f999e4e
Fard-Faru <fardin2273faruk@gmail.com>
2022-08-21 16:25:59 -0400

    Added all Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AboutMeComponent } from './about-me/about-me.component';
+import { EducationComponent } from './education/education.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ExtracurricularsComponent } from './extracurriculars/extracurriculars.component';
+import { AwardsComponent } from './awards/awards.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutMeComponent
+    AboutMeComponent,
+    EducationComponent,
+    ExperienceComponent,
+    ProjectsComponent,
+    SkillsComponent,
+    ExtracurricularsComponent,
+    AwardsComponent
   ],
   imports: [
     BrowserModule
